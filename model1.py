bad9962315ac3aab2d35c7473de3b5b3de55b018
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-05-18 21:18:00 -0500

    commit de modelos

--- model1.py
+++ model1.py
@@ -55,3 +55,89 @@ def show_roc_hot(matriz_confusion):
     for i in range(len(matriz_confusion)):
         sns.heatmap(matriz_confusion[i])
     plt.show()
+
+
+def sh_me(str_model, AUC, acc_validation, acc_test, y_test, y_pred):
+    # show metrics
+    print('-' * 50 + '\n')
+    print(str.upper(str_model))
+    print('\n')
+    print(f'Accuracy de validación: {acc_validation} ')
+    print(f'Accuracy de test: {acc_test} ')
+    print(classification_report(y_test, y_pred))
+    print(f'AUC: {AUC} ')
+
+
+def panda_values(data):
+    columns = ['Accuracy de Entrenamiento', 'Accuracy de Validación',
+               'Accuracy de Test', 'Recall del Modelo', 'Precisión del Modelo',
+               'F1-Score del Modelo', 'Área bajo la Curva (AUC)']
+    data = np.transpose(data)
+    tabla = pd.DataFrame(data=data, index=model_name, columns=columns)
+    return tabla.sort_values(by=['Área bajo la Curva (AUC)'], ascending=False)
+
+
+def view_matriz_confusion(matriz_confusion):
+    for i in range(len(matriz_confusion)):
+        print(model_name[i])
+        print(pd.DataFrame(matriz_confusion[i]))
+        print('\n')
+
+
+def classifier_int(columns):
+    for column in columns:
+        maximo = int(max(data[column].unique())) + 1
+        minimo = min(data[column].unique())
+        size = maximo - minimo
+        text = ['1', '2', '3', '4', '5', '6', '7']
+        rangos = [((size/len(text))*n) + minimo for n in range(len(text) + 1)]
+        data[column] = pd.cut(data[column], rangos, labels=text)
+
+
+url = 'DataSets/Prima Indians Diabetes Database/diabetes.csv'
+data = pd.read_csv(url)
+
+# manejo de datos
+data.Outcome.value_counts(dropna=False)
+
+# Volver categoricos los datos
+
+classifier_int(['Glucose', 'BloodPressure', 'SkinThickness',
+                'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
+
+data.dropna(axis=0, how='any', inplace=True)
+# machine learning
+x = np.array(data.drop(['Outcome'], 1))
+y = np.array(data.Outcome)  # 0 sin diabetes, 1 con diabetes
+
+# x_train, x_test, y_train, y_test
+x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size=0.2)
+
+# metricas
+model_name = ['LOGISTIC REGRESSION', 'DECISION TREE', 'KNEIGHBORNS',
+              'RANDOM FOREST CLASSIFIER', 'GRADIENT BOOSTING CLASSIFIER']
+
+acc_va = acc_te = recall = precision = f1 = auc = list(range(len(model_name)))
+matriz_confu = list(range(len(model_name)))
+model = [LogisticRegression(), DecisionTreeClassifier(),
+         KNeighborsClassifier(n_neighbors=3), RandomForestClassifier(),
+         GradientBoostingClassifier()]
+
+for i in range(len(model_name)):
+    # model, acc_validation, acc_test, y_pred
+    model[i], vacc_va, vacc_te, y_pr = me(model[i], x_tr, x_te, y_tr, y_te)
+    matriz_confusion, vauc = mca(model[i], x_te, y_te, y_pr)
+    # valores de la matriz de confusion
+    recall[i] = recall_score(y_te, y_pr)
+    precision[i] = precision_score(y_te, y_pr)
+    f1[i] = f1_score(y_te, y_pr)
+    auc[i] = vauc
+    acc_va[i] = vacc_va
+    acc_te[i] = vacc_te
+    # matriz de confusion
+    matriz_confu[i] = matriz_confusion
+
+tabla = panda_values([acc_va, acc_va, acc_te, recall, precision, f1, auc])
+view_matriz_confusion(matriz_confu)
+show_roc_hot(matriz_confu)
+print(tabla)
--- model2.py
+++ model2.py
@@ -56,3 +56,117 @@ def show_roc_hot(matriz_confusion):
     for i in range(len(matriz_confusion)):
         sns.heatmap(matriz_confusion[i])
     plt.show()
+
+
+def show_roc_curve_matrix(model, x_test, y_test):
+    colors = ['orange', 'blue', 'yellow', 'green', 'red', 'silver']
+    # show plot ROC
+    for i in range(len(model)):
+        fpr, tpr = fpr_tpr(model[i], x_test, y_test)
+        # sns.heatmap(matriz_confusion)
+        # plt.show()
+        plt.plot(fpr, tpr, color=colors[i], label='ROC')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(model_name + ['line'])
+    plt.show()
+
+
+def sh_me(str_model, AUC, acc_validation, acc_test, y_test, y_pred):
+    # show metrics
+    print('-' * 50 + '\n')
+    print(str.upper(str_model))
+    print('\n')
+    print(f'Accuracy de validación: {acc_validation} ')
+    print(f'Accuracy de test: {acc_test} ')
+    print(classification_report(y_test, y_pred))
+    print(f'AUC: {AUC} ')
+
+
+def convert_data_categ(data, columns):
+    # convert data to category
+    for column in columns:
+        rows = list(data[column].unique())
+        new_rows = list(range(len(rows)))
+        data.replace(rows, new_rows, inplace=True)
+
+
+def panda_values(data):
+    columns = ['Accuracy de Entrenamiento', 'Accuracy de Validación',
+               'Accuracy de Test', 'Recall del Modelo', 'Precisión del Modelo',
+               'F1-Score del Modelo', 'Área bajo la Curva (AUC)']
+    data = np.transpose(data)
+    tabla = pd.DataFrame(data=data, index=model_name, columns=columns)
+    return tabla.sort_values(by=['Área bajo la Curva (AUC)'], ascending=False)
+
+
+def view_matriz_confusion(matriz_confusion):
+    for i in range(len(matriz_confusion)):
+        print(model_name[i])
+        print(pd.DataFrame(matriz_confusion[i]))
+        print('\n')
+
+
+url = 'DataSets/Bank Marketing/bank-full.csv'
+data = pd.read_csv(url)
+
+# manejo de datos
+data.y.value_counts(dropna=False)
+
+columns = ['job', 'marital', 'default', 'loan', 'contact', 'y']
+
+# Volver categoricos los datos
+convert_data_categ(data, columns)
+
+# caso unico (education)
+rows = ['tertiary', 'secondary', 'primary']
+new_rows = [3, 2, 1]
+data.education.replace(rows, new_rows, inplace=True)
+
+rangos = [0, 8, 15, 18, 25, 40, 60, 100]
+nombres = ['1', '2', '3', '4', '5', '6', '7']
+data.age = pd.cut(data.age, rangos, labels=nombres)
+# contact, day, month, duration, previous
+columns_delete = ['contact', 'day', 'month', 'duration',
+                  'previous', 'pdays', 'balance', 'poutcome']
+data.drop(columns_delete, axis=1, inplace=True)
+data.dropna(axis=0, how='any', inplace=True)
+
+# machine learning
+x = np.array(data.drop(['y'], 1))
+y = np.array(data.y)  # 0 no suscrito, 1 suscrito
+
+# x_train, x_test, y_train, y_test
+x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size=0.2)
+
+# metricas
+model_name = ['LOGISTIC REGRESSION', 'DECISION TREE', 'KNEIGHBORNS',
+              'RANDOM FOREST CLASSIFIER', 'GRADIENT BOOSTING CLASSIFIER']
+
+acc_va = acc_te = recall = precision = f1 = auc = list(range(len(model_name)))
+matriz_confu = list(range(len(model_name)))
+model = [LogisticRegression(), DecisionTreeClassifier(),
+         KNeighborsClassifier(n_neighbors=3), RandomForestClassifier(),
+         GradientBoostingClassifier()]
+
+for i in range(len(model_name)):
+    # model, acc_validation, acc_test, y_pred
+    model[i], vacc_va, vacc_te, y_pr = me(model[i], x_tr, x_te, y_tr, y_te)
+    matriz_confusion, vauc = mca(model[i], x_te, y_te, y_pr)
+    # valores de la matriz de confusion
+    recall[i] = recall_score(y_te, y_pr)
+    precision[i] = precision_score(y_te, y_pr)
+    f1[i] = f1_score(y_te, y_pr)
+    auc[i] = vauc
+    acc_va[i] = vacc_va
+    acc_te[i] = vacc_te
+    # matriz de confusion
+    matriz_confu[i] = matriz_confusion
+
+tabla = panda_values([acc_va, acc_va, acc_te, recall, precision, f1, auc])
+view_matriz_confusion(matriz_confu)
+show_roc_hot(matriz_confu)
+print(tabla)
+show_roc_curve_matrix(model, x_te, y_te)
